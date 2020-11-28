03748fd6c1c65553083c24594f00fa995bf76440
Steven Petteruti <stevenpetterutijr@gmail.com>
2020-11-28 01:01:54 -0500

    running .ts worker

--- src/index.ts
+++ src/index.ts
@@ -32,7 +32,7 @@ const { Pool, Client } = require('pg')
 const uri = 'postgres://edwfxtxadowqjw:3dc337268b226f9b4ee934a5c817c3a5e9517c65ea07779a6438f63f92a53d8b@ec2-54-158-190-214.compute-1.amazonaws.com:5432/dajno1b88amgs9?ssl=no-verify'
 
 
-
+console.log(`running ts worker`)
 // add
 async function main() {
     const runner = await run({
