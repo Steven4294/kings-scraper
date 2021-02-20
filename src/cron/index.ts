9590d3877f1a9cb4f7363de34e6eb20f9605e617
Steven Petteruti <stevenpetterutijr@gmail.com>
2021-02-20 01:21:17 -0500

    push

--- src/cron/index.ts
+++ src/cron/index.ts
@@ -1,47 +1,15 @@
 import * as schedule from "node-schedule";
-import Store from '../db/models/Store'
-const { run, quickAddJob } = require("graphile-worker");
-const uri = 'postgres://edwfxtxadowqjw:3dc337268b226f9b4ee934a5c817c3a5e9517c65ea07779a6438f63f92a53d8b@ec2-54-158-190-214.compute-1.amazonaws.com:5432/dajno1b88amgs9?ssl=no-verify'
 
 class job {
 
     constructor() { }
 
-    public async  shopRefresh()  {
-        await this.delay(1000)
+    public async runJob()  {
+        // 
+        schedule.scheduleJob('* * * * *', async () => {
+            console.log('ping....')
 
-        const stores = await Store.findAll()
-
-
-        stores.map(async store => {
-            console.log(`-------------`)
-            console.log('install store')
-            console.log(`-------------`)
-            console.log(`payload ${store.name}`)
-            await quickAddJob(
-                { connectionString: uri },
-                "helloWorld", // Task identifier
-            );
-            await quickAddJob(
-                { connectionString: uri },
-                "installStore", // Task identifier
-                { payload: store }, // payload
-            );
-        })
-       
-        var rule = new schedule.RecurrenceRule();
-        // rule.dayOfWeek = [0, new schedule.Range(0, 6)];
-        rule.second = 1;
-        //rule.minute = 0;
-        // schedule.scheduleJob('0 0 * * * *', async () => {
-        //     stores.map(async store => {
-        //         await quickAddJob(
-        //             { connectionString: uri },
-        //             "installStore", // Task identifier
-        //             { payload: store }, // payload
-        //         );
-        //     })
-        // });
+        });
     }
 
     async delay(ms: number) {
--- src/index.ts
+++ src/index.ts
@@ -1,188 +1,166 @@
 require('@babel/polyfill');
 
 import { sequelize } from './db'
-import { getProductsPoll } from './tasks/getProductsPoll'
-import { installStore } from './tasks/installStore';
 import Store from './db/models/Store'
-import Cart from './db/models/Cart'
-import { klaviyoEvent } from './tasks/klaviyoEvent';
-import ProductVariant from './db/models/ProductVariant';
-import { abandonedCheckoutsTask } from './tasks/abandonedCheckouts';
 const { run, quickAddJob } = require("graphile-worker");
+require('chromedriver');
+var webdriver = require('selenium-webdriver');
+const {Builder, By, Key, until} = require('selenium-webdriver');
 import job from './cron'
-import { klaviyoPing } from './tasks/klaviyoPing';
-import { klaviyoAbandonedCartJsonForCart } from './helpers'
+var driver = new webdriver.Builder()
+  .forBrowser('chrome')
+  .build();
 
-const uri = 'postgres://edwfxtxadowqjw:3dc337268b226f9b4ee934a5c817c3a5e9517c65ea07779a6438f63f92a53d8b@ec2-54-158-190-214.compute-1.amazonaws.com:5432/dajno1b88amgs9?ssl=no-verify'
 
-sequelize.sync()
+// const uri = 'postgres://edwfxtxadowqjw:3dc337268b226f9b4ee934a5c817c3a5e9517c65ea07779a6438f63f92a53d8b@ec2-54-158-190-214.compute-1.amazonaws.com:5432/dajno1b88amgs9?ssl=no-verify'
+
+// sequelize.sync()
 
 console.log(`running ts worker`)
 // add
 async function main() {
-    const runner = await run({
-        connectionString: uri,
-        concurrency: 5,
-        maxPoolSize: 10,
-        noHandleSignals: false,
-        pollInterval: 1000,
+    // const runner = await run({
+    //     connectionString: uri,
+    //     concurrency: 5,
+    //     maxPoolSize: 10,
+    //     noHandleSignals: false,
+    //     pollInterval: 1000,
 
-        taskList: {
-            productUpdate: async (payload: { productVariantDic: any; domain: any; }, helpers: any) => {
-				  const { productVariantDic, domain } = payload;
-				  console.log(`${JSON.stringify(productVariantDic)}`)           
-			  	const ids = productVariantDic.map((variant: { id: any; }) => variant.id)
-				const variants: ProductVariant[] = await ProductVariant.findAll({
-					where: {
-						id: ids
-					}
-				})
+    //     taskList: {
+	// 		helloWorld: async () => {
+	// 			console.log('')
+	// 			console.log('hello world is called!!!')
+	// 			console.log('')
+	// 		}
+    //     }
+    // });
 
-				if (variants.length === 0) {
-					productVariantDic.map((variant: any) => {
-						ProductVariant.create({
-							id: variant.id,
-							price: variant.price
-						})
-					})
-				}
-				const store = await Store.findOne({
-					where: {
-						id: domain
-					}
-				})
 
-				const oldDic = variants.map(v => {return { id: v.id, price: v.price }})
-				const deltas = oldDic.map(v => {
-				 	const matches = productVariantDic.filter((v2: { id: string; }) => v2.id === v.id)
-				  	if (matches === undefined || matches.length === 0) { return }
-					const match = matches[0]
-					const newPrice = parseFloat(match.price)
-					const oldPrice = parseFloat(v.price);
-
-					console.log(` ${oldPrice} -> ${newPrice}`)
-					if (shouldSendKlaviyoEmail(store!, newPrice, oldPrice)) {
-						console.log(`PRICE DROP: ${v.id} ${oldPrice} -> ${newPrice}`)
-						return {
-							id: match.id,
-							price_old: v.price,
-							price_new: match.price,
-						}
-					} else {
-						// it was a price hike!
-						ProductVariant.update({
-							price: match.price
-						}, {
-							where: { id: match.id },
-						})
-					}
-				}).filter(v => v !== undefined)
-				if (deltas.length === 0) { 
-					console.log('no emails to be sent')
-					return
-				 }
-
-				console.log(` >>>> domain = ${domain} || ${store} ${deltas}`)
-
-				// logic to see if we should send the klaviyo event
-					await quickAddJob( 
-						{ connectionString: uri },
-						"sendKlaviyoEvents", // Task identifier
-						{ 
-							payload: {
-								store: store,
-								deltas: deltas,
-							}
-						});
-				
-
-            },
-            getProducts: getProductsPoll,
-            installStore: installStore,
-			sendKlaviyoEvents: klaviyoEvent,
-			klaviyoPing: klaviyoPing,
-			abandonedCheckouts: abandonedCheckoutsTask,
-			helloWorld: async () => {
-				console.log('')
-				console.log('hello world is called!!!')
-				console.log('')
-			}
-        }
-    });
-
-	setTimeout(async () => {
-		const stores = await Store.findAll()
-		stores.map(async store => {
-			// await quickAddJob(
-			// 	{ connectionString: uri },
-			// 	"klaviyoPing", // Task identifier
-			// 	{ payload: store }, // payload
-			// );
-		})
-
-	}, 1000)
-
-    // setTimeout(async () => {
-
-	// const store: Store = new Store({id: 'zeiger-5.myshopify.com', name: 'zeiger-5.myshopify.com', accessToken: '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'})
-	// console.log(` >>> SHOULD BE INIT() JOBS <<<`)
-    // await quickAddJob(
-    //     { connectionString: uri },
-    //     "installStore", // Task identifier
-    //     { payload: store }, // payload
-    // );
-
-    // }, 1000)
+ 
 }
 
-async function testKlaviyoJson() {
-	// const product = await ProductVariant.findOne({
-	// 	where: {
-	// 	  id: id
-	// 	},
-	// 	  include: [{
-	// 	  model: Cart,
-	// 	  as: 'carts',
-	// 	  required: false,
-	// 	  attributes: ['id', 'email', 'abandoned_checkout_url'],
-	// 	}]
-	//   })
 
-	// const carts = await Cart.findAll({
-	// 	include: [{
-	// 		model: ProductVariant,
-	// 		as: 'products'
-	// 	}]
-	// });
-	// const cart = carts[0]
-	// const klaviyo = klaviyoAbandonedCartJsonForCart(cart)
-	
-	// console.log(klaviyo.extra.line_items)
+const username = 'Eugene L'
+const password = '123'
+const url = 'https://kingsclubpkr.com/'
+
+function delay(ms: number) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
 }
 
-function shouldSendKlaviyoEmail(store: Store, newPrice: number, oldPrice: number) {
-	const diff = Math.abs(newPrice - oldPrice)
-	const percentDiff = (diff / oldPrice) * 100.0
+async function helloWorld() {
+  try {
+	await driver.get(url);
+	await driver.findElement(By.xpath('/html/body/div[19]/div[1]/div/input[1]')).sendKeys(username)
+	await driver.findElement(By.xpath('/html/body/div[19]/div[1]/div/input[2]')).sendKeys(password, Key.RETURN)
+	await delay(500)
+	const playButton = await driver.findElement(By.xpath('/html/body/div[21]/div[1]/div/span[2]'))
+	await delay(500)
+	playButton.click()
+	// await delay(2000)
+	const closeButton = await driver.findElement(By.xpath('/html/body/div[6]/div[3]/span'))
+	// await delay(2000)
+	closeButton.click()
+	 await delay(200)
 
-	const b1 = diff >= (store.limitAmount ?? 10)
-	const b2 = percentDiff >= (store.limitPercent ?? 10)
+	const results = await getTables_v2()
+	// await getTables()
 
-	console.log(`b1: ${b1},  b2: ${b2}   percent ${percentDiff}`)
-	if (store.amountAndPercent === true) {
-		// AND
-		return b1 && b2
-	} else {
-		// OR
-		return b1 || b2
+    // await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
+    // await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+    } finally {
+    
 	}
 }
 
-testKlaviyoJson()
+async function getTables_v2() {
+
+	const arr = Array.from({length: 10}, (_, i) => i + 1) // [1, 2, .., N]
+	arr.map(async r => {
+		await delay(1000)
+		console.log('test mcgee')
+	})
+	var results: String[] = []
+	const r1 = await getTable(1, true)
+	const r2 = await getTable(2)
+	const r3 = await getTable(3)
+	const r4 = await getTable(4)
+	const r5 = await getTable(5)
+	const r6 = await getTable(6)
+	const r7 = await getTable(7)
+	const r8 = await getTable(8)
+
+	const arrs = [r1, r2, r3, r4, r5, r6, r7, r8]
+	arrs.map(elem => {
+		results = results.concat(elem)
+	})
+	console.log(`~~~~~~~~`)
+	console.log(results)
+	return new Promise<String[]>((resolve, reject) => {
+		resolve(results)
+	})
+}
+
+async function getTable(index: number, isFirst = false): Promise<String[]> {
+	await driver.findElement(By.xpath(`/html/body/div[14]/div[7]/div[1]/div[1]/div[2]/div/div[${index}]/div[1]`)).click()
+	if (isFirst) {
+		await delay(1200)
+	} else { await delay(400) }
+	await driver.findElement(By.xpath(`/html/body/div[14]/div[7]/div[1]/div[1]/div[2]/div/div[${index}]/div[1]`)).click()
+	await delay(100)
+	await driver.findElement(By.xpath(`/html/body/div[14]/div[7]/div[1]/div[1]/div[2]/div/div[${index}]/div[1]`)).click()
+
+	return getScreennames()
+}
+
+async function getTables() {
+	// const arr = Array.from({length: 10}, (_, i) => i + 1) // [1, 2, .., N]
+	// const promises = arr.map(async n => {
+	// 	await getTable(n)
+	// })
+	// await Promise.all(promises)
+	const r1 = await getTable(1)
+	await getTable(2)
+	await getTable(3)
+	await getTable(4)
+	await getTable(5)
+	await getTable(6)
+
+}
+
+function getScreennames(): Promise<String[]> {
+	return new Promise<String[]>((resolve, reject) => {
+		const results: String[] = []
+		const x1 = '/html/body/div[14]/div[7]/div[2]/div[2]/div/div[1]/div[1]/span[1]'
+		const x2 = '/html/body/div[14]/div[7]/div[2]/div[2]/div/div[1]/div[2]/span[1]'
+		const x3 = '/html/body/div[14]/div[7]/div[2]/div[2]/div/div[1]/div[3]/span[1]'
+		const x4 = '/html/body/div[14]/div[7]/div[2]/div[2]/div/div[1]/div[4]/span[1]'
+		const x5 = '/html/body/div[14]/div[7]/div[2]/div[2]/div/div[1]/div[5]/span[1]'
+		const x6 = '/html/body/div[14]/div[7]/div[2]/div[2]/div/div[1]/div[6]/span[1]'
+		const x7 = '/html/body/div[14]/div[7]/div[2]/div[2]/div/div[1]/div[7]/span[1]'
+		const arr = [x1, x2, x3, x4, x5, x6, x7]
+
+		arr.map(async xpath => {
+		try {
+			const elem = await driver.findElement(By.xpath(xpath))
+			const text = await elem.getText()
+			results.push(text)
+		} finally {
+		}
+		})
+		resolve(results)
+
+	});
+
+
+}
+
+//helloWorld()
 
 main().catch((err) => {
 	console.error(err);
     process.exit(1);
 }).then(() => {
-	// job.shopRefresh()
+	job.runJob()
 })
 
