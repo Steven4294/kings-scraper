e26225dee6a3563208337b7a34ee020f9f6a91de
Steven Petteruti <stevenpetterutijr@gmail.com>
2022-02-18 16:22:57 -0500

    push

--- src/index.ts
+++ src/index.ts
@@ -16,9 +16,9 @@ options.setChromeBinaryPath(process.env.CHROME_BINARY_PATH);
 let serviceBuilder = new chrome.ServiceBuilder(process.env.CHROME_DRIVER_PATH);
 
 //Don't forget to add these for heroku
-// options.addArguments("--headless");
-// options.addArguments("--disable-gpu");
-// options.addArguments("--no-sandbox");
+options.addArguments("--headless");
+options.addArguments("--disable-gpu");
+options.addArguments("--no-sandbox");
 
 
 let driver = new webdriver.Builder()
@@ -27,42 +27,20 @@ let driver = new webdriver.Builder()
 	.setChromeService(serviceBuilder)
 	.build();
 
-
-// var driver = new webdriver.Builder()
-//   .forBrowser('chrome')
-//   .build();
-
-
-// const uri = 'postgres://edwfxtxadowqjw:3dc337268b226f9b4ee934a5c817c3a5e9517c65ea07779a6438f63f92a53d8b@ec2-54-158-190-214.compute-1.amazonaws.com:5432/dajno1b88amgs9?ssl=no-verify'
-
-// sequelize.sync()
-
-console.log(`running ts worker`)
-// add
 async function main() {
-    // const runner = await run({
-    //     connectionString: uri,
-    //     concurrency: 5,
-    //     maxPoolSize: 10,
-    //     noHandleSignals: false,
-    //     pollInterval: 1000,
-
-    //     taskList: {
-	// 		helloWorld: async () => {
-	// 			console.log('')
-	// 			console.log('hello world is called!!!')
-	// 			console.log('')
-	// 		}
-    //     }
-    // });
-
-
+ 
  
 }
 
+interface WhaleWatcherPlayer {
+	name: String
+    table: String
+    stakes: String
+}
 
-const username = 'Eugene L'
-const password = '123'
+
+const username = 'JohnT1'
+const password = 'pw'
 const url = 'https://kingsclubpkr.com/'
 const base = 'https://zeiger-whalewatcher.herokuapp.com'
 // const base = 'http://127.0.0.1:8080'
@@ -73,22 +51,28 @@ function delay(ms: number) {
     return new Promise( resolve => setTimeout(resolve, ms) );
 }
 
-async function scrapeKings() {
-	const closeButton = await driver.findElement(By.xpath('/html/body/div[6]/div[3]/span'))
-	// await delay(2000)
-	closeButton.click()
-	 await delay(200)
+async function scrapeKings(initial: boolean) {
+	if (initial === true) {
+		const closeButton = await driver.findElement(By.xpath('/html/body/div[6]/div[3]/span'))
+		// await delay(2000)
+		closeButton.click()
+		 await delay(300)
+	}
 
 	const results = await getTables_v2()
 	// await getTables()
-
+	console.log("-----")
+	console.log(results)
+	console.log("-----")
 	var options = {
 		'method': 'POST',
 		'url': post_url,
 		'headers': {
 		  'Content-Type': 'application/json'
 		},
-		body: JSON.stringify({"message":`${results}`})  
+		body: JSON.stringify({
+			"type":"Kingsclub",
+		"players": results})  
 	};
 	request(options);
 }
@@ -96,10 +80,13 @@ async function scrapeKings() {
 async function loadKings() {
   try {
 	await driver.get(url);
-	await driver.findElement(By.xpath('/html/body/div[19]/div[1]/div/input[1]')).sendKeys(username)
-	await driver.findElement(By.xpath('/html/body/div[19]/div[1]/div/input[2]')).sendKeys(password, Key.RETURN)
+	await delay(3500)
+// /html/body/div[18]/div[1]/div/input[1]
+	await driver.findElement(By.xpath('/html/body/div[18]/div[1]/div/input[1]')).sendKeys(username)
+	await driver.findElement(By.xpath('/html/body/div[18]/div[1]/div/input[2]')).sendKeys(password, Key.RETURN)
 	await delay(500)
-	const playButton = await driver.findElement(By.xpath('/html/body/div[21]/div[1]/div/span[2]'))
+	// /html/body/div[20]/div[1]/div/span[2]
+	const playButton = await driver.findElement(By.xpath('/html/body/div[20]/div[1]/div/span[2]'))
 	await delay(500)
 	playButton.click()
 
@@ -118,7 +105,7 @@ async function getTables_v2() {
 	// 	await delay(1000)
 	// 	console.log('test mcgee')
 	// })
-	var results: String[] = []
+	var results: WhaleWatcherPlayer[] = []
 	const r1 = await getTable(1, true)
 	const r2 = await getTable(2)
 	const r3 = await getTable(3)
@@ -134,48 +121,54 @@ async function getTables_v2() {
 	})
 	console.log(`~~~~~~~~`)
 	console.log(results)
-	return new Promise<String[]>((resolve, reject) => {
+	return new Promise<WhaleWatcherPlayer[]>((resolve, reject) => {
 		resolve(results)
 	})
 }
 
-async function getTable(index: number, isFirst = false): Promise<String[]> {
+async function getTable(index: number, isFirst = false): Promise<WhaleWatcherPlayer[]> {
 	try {
-		await driver.findElement(By.xpath(`/html/body/div[14]/div[7]/div[1]/div[1]/div[2]/div/div[${index}]/div[1]`)).click()
+		// /html/body/div[13]/div[7]/div[1]/div[1]/div[2]/div/div[1]
+		await driver.findElement(By.xpath(`/html/body/div[13]/div[7]/div[1]/div[1]/div[2]/div/div[${index}]/div[1]`)).click()
 		await delay(300)
-		await driver.findElement(By.xpath(`/html/body/div[14]/div[7]/div[1]/div[1]/div[2]/div/div[${index}]/div[1]`)).click()
+		await driver.findElement(By.xpath(`/html/body/div[13]/div[7]/div[1]/div[1]/div[2]/div/div[${index}]/div[1]`)).click()
 		if (isFirst) {
 			await delay(1200)
 		} else { await delay(1000) }
 		
-		await driver.findElement(By.xpath(`/html/body/div[14]/div[7]/div[1]/div[1]/div[2]/div/div[${index}]/div[1]`)).click()
+		await driver.findElement(By.xpath(`/html/body/div[13]/div[7]/div[1]/div[1]/div[2]/div/div[${index}]/div[1]`)).click()
 		await delay(300)
 	} finally { }
 
 	return getScreennames()
 }
 
-function getScreennames(): Promise<String[]> {
-	return new Promise<String[]>((resolve, reject) => {
-		const results: String[] = []
+function getScreennames(): Promise<WhaleWatcherPlayer[]> {
+	return new Promise<WhaleWatcherPlayer[]>((resolve, reject) => {
+		const results: WhaleWatcherPlayer[] = []
 
-		const x1 = '/html/body/div[14]/div[7]/div[2]/div[2]/div/div[1]/div[1]/span[1]'
-		const x2 = '/html/body/div[14]/div[7]/div[2]/div[2]/div/div[1]/div[2]/span[1]'
-		const x3 = '/html/body/div[14]/div[7]/div[2]/div[2]/div/div[1]/div[3]/span[1]'
-		const x4 = '/html/body/div[14]/div[7]/div[2]/div[2]/div/div[1]/div[4]/span[1]'
-		const x5 = '/html/body/div[14]/div[7]/div[2]/div[2]/div/div[1]/div[5]/span[1]'
-		const x6 = '/html/body/div[14]/div[7]/div[2]/div[2]/div/div[1]/div[6]/span[1]'
-		const x7 = '/html/body/div[14]/div[7]/div[2]/div[2]/div/div[1]/div[7]/span[1]'
+		const x1 = '/html/body/div[13]/div[7]/div[2]/div[2]/div/div[1]/div[1]/span[1]'
+		const x2 = '/html/body/div[13]/div[7]/div[2]/div[2]/div/div[1]/div[2]/span[1]'
+		const x3 = '/html/body/div[13]/div[7]/div[2]/div[2]/div/div[1]/div[3]/span[1]'
+		const x4 = '/html/body/div[13]/div[7]/div[2]/div[2]/div/div[1]/div[4]/span[1]'
+		const x5 = '/html/body/div[13]/div[7]/div[2]/div[2]/div/div[1]/div[5]/span[1]'
+		const x6 = '/html/body/div[13]/div[7]/div[2]/div[2]/div/div[1]/div[6]/span[1]'
+		const x7 = '/html/body/div[13]/div[7]/div[2]/div[2]/div/div[1]/div[7]/span[1]'
 		const arr = [x1, x2 , x3, x4, x5, x6, x7]
 
 		arr.map(async xpath => {
-		try {
-			const elem = await driver.findElement(By.xpath(xpath))
-			const text = await elem.getText()
-			results.push(text)
-		} finally {
-			resolve(results)
-		}
+			try {
+				const elem = await driver.findElement(By.xpath(xpath))
+				const text = await elem.getText()
+				await delay(300)
+				results.push({
+					name: text,
+					stakes: '',
+					table: '',
+				})
+			} catch {
+				resolve(results)
+			}
 		})
 		resolve(results)
 	});
@@ -187,8 +180,11 @@ main().catch((err) => {
 }).then(async () => {
 	await loadKings()
 
-	schedule.scheduleJob("*/40 * * * * *", async () => {
+	var initial = true
+	schedule.scheduleJob("*/70 * * * * *", async () => {
 		//every 40 seconds
-		await scrapeKings()
+		console.log(initial)
+		await scrapeKings(initial)
+		initial = false
 	});
 })
